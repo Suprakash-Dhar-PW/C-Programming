55dd0badaa22d045b896e9b6fec2276d383e32de
Suprakash-Dhar-PW <dharsuprakash02@gmail.com>
2025-02-15 23:41:21 +0530

    Add diamond-isoceles-trianglr file

---
+++ Day 1/diamond-isoceles-triangle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=5;++i){
+        for(int k=1;k<=5-i+1;++k){
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;++j){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i=2;i<=5;++i){
+        for(int k=1;k<=i;++k){
+            printf(" ");
+        }
+        for(int j=1;j<=2*(5-i+1)-1;++j){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
